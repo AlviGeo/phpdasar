e49e2ba5a6f690056db8fd5de972114d0cb41e39
AlviGeo <alvigeovan29@gmail.com>
2020-08-04 10:12:10 +0700

    done

--- pertemuan12/tambah.php
+++ pertemuan12/tambah.php
@@ -1,28 +1,23 @@
 <?php
 require 'functions.php';
 
-// ambil data di URL
-$id = $_GET["id"];
-
-// query data mahasiswa berdasarkan id
-$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
 
 // cek apakah tombol submit sudah ditekan atau belum 
 if( isset($_POST["submit"])) {
 
 
-    // cek apakah data berhasil diubah atau tidak
-    if( ubah($_POST) > 0 ) {
+    // cek apakah data berhasil ditambahkan atau tidak
+    if( tambah($_POST) > 0 ) {
         echo "
             <script>
-                alert('Data Berhasil Diubah!');
+                alert('data berhasil ditambahkan!');
                 document.location.href = 'index.php';
             </script>
         ";
     } else {
         echo "
             <script>
-                alert('Data Gagal Diubah!');
+                alert('data gagal ditambahkan!');
                 document.location.href = 'index.php';
             </script>
         ";
@@ -35,41 +30,35 @@ if( isset($_POST["submit"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ubah data mahasiswa</title>
+    <title>Tambah data mahasiswa</title>
 </head>
 <body>
-    <h1>Ubah data mahasiswa</h1>
+    <h1>Tambah data mahasiswa</h1>
 
     <form action="" method="post">  
         <ul>
-            <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
             <li>
                 <label for="nrp" >NRP : </label>
-                <input type="text" name="nrp" id="nrp" required
-                value="<?= $mhs["nrp"]; ?>" >
+                <input type="text" name="nrp" id="nrp" required>
             </li>
             <li>
                 <label for="nama">Nama : </label>
-                <input type="text" name="nama" id="nama" 
-                value="<?= $mhs["nama"]; ?>">
+                <input type="text" name="nama" id="nama">
             </li>
             <li>
                 <label for="email">Email : </label>
-                <input type="text" name="email" id="email"
-                value="<?= $mhs["email"]; ?>">
+                <input type="text" name="email" id="email">
             </li>
             <li>
                 <label for="jurusan">Jurusan : </label>
-                <input type="text" name="jurusan" id="jurusan"
-                value="<?= $mhs["jurusan"]; ?>">
+                <input type="text" name="jurusan" id="jurusan">
             </li>
             <li>
                 <label for="gambar">Gambar : </label>
-                <input type="text" name="gambar" id="gambar"
-                value="<?= $mhs["gambar"]; ?>">
+                <input type="text" name="gambar" id="gambar">
             </li>
             <li>
-                <button type="submit" name="submit">Ubah Data!</button>
+                <button type="submit" name="submit">Tambah Data!</button>
             </li>
         </ul>
 
